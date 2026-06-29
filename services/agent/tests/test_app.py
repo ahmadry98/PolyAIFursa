5ebf12f28c3c4efe1777f98440efd9690a5272ac
Ahmad Rayan <ahmadrayan1998@gmail.com>
2026-06-29 17:11:37 +0300

    Address pytest import path review and all the other comments

--- services/agent/tests/test_app.py
+++ services/agent/tests/test_app.py
@@ -1,11 +1,7 @@
-import os
-import sys
-
+import app
 from fastapi.testclient import TestClient
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-import app
 
 client = TestClient(app.app)
 
